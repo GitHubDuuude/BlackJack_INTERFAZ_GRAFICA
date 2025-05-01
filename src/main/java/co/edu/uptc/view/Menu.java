1c564bca217211ed8596939b88bd3381ba2c90a2
AndrewzZzsf <sotofuyadiego@gmail.com>
2025-05-01 11:49:56 -0500

    XD

--- src/main/java/co/edu/uptc/view/Menu.java
+++ src/main/java/co/edu/uptc/view/Menu.java
@@ -2,6 +2,7 @@ package co.edu.uptc.view;
 
 public class Menu {
     public static void main(String[] args) {
-        System.out.println("hola gente de peru");
+        System.out.println("sadasdasdahola gente de peru");
+
     }
 }
